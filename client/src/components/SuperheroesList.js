ec5e34e149e5484c645faaa8ff153e1aeda49236
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-10 16:32:33 +0300

    routing for two page

--- client/src/components/SuperheroesList.js
+++ client/src/components/SuperheroesList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { httpRequest } from '../helpers/http.helper';
+
 export default function SuperheroesList() {
+  const { request } = httpRequest();
+
   return (
     <div>
       <h1>Superheroes</h1>
     </div>
   )
-}
+};
+
