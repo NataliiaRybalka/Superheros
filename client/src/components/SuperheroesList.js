22c5b47aa653626be29d8f201cd61510fca1b10a
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-10 17:21:00 +0300

    get all heroes on client

--- client/src/components/SuperheroesList.js
+++ client/src/components/SuperheroesList.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { httpRequest } from '../helpers/http.helper';
 
 export default function SuperheroesList() {
   const { request } = httpRequest();
 
+  const [allSuperheroes, setAllSuperheroes] = useState();
+
+  const getAllSuperheroes = async () => {
+    const data = await request('http://localhost:5000/');
+    setAllSuperheroes(data);
+  };
+
+  useEffect(() => {
+    getAllSuperheroes();
+  }, []);
+
   return (
     <div>
       <h1>Superheroes</h1>
+
+      {!!allSuperheroes && allSuperheroes.map(superhero => (
+        <div key={superhero.id}>
+          <h3>{superhero.nickname}</h3>
+        </div>
+      ))}
     </div>
   )
 };
