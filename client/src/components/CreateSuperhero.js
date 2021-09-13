116fe516f866523136e8f4c8c2a5efe2f4efc155
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-13 14:05:40 +0300

    css form

--- client/src/components/CreateSuperhero.js
+++ client/src/components/CreateSuperhero.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router';
 import axios from 'axios';
+import './CreateSuperhero.css';
 
 export default function CreateSuperhero() {
   const [state, setState] = useState({
@@ -56,25 +57,29 @@ export default function CreateSuperhero() {
 
   return (
     <div>
-      <label>nickname</label>
-      <input value={state.nickname} onChange={changeInput} type={'text'} name={'nickname'} />
-      <br />
-      <label>real_name</label>
-      <input value={state.real_name} onChange={changeInput} type={'text'} name={'real_name'} />
-      <br />
-      <label>origin_description</label>
-      <textarea value={state.origin_description} onChange={changeInput} name={'origin_description'} />
-      <br />
-      <label>superpowers</label>
-      <input value={state.superpowers} onChange={changeInput} type={'text'} name={'superpowers'} />
-      <br />
-      <label>catch_phrase</label>
-      <input value={state.catch_phrase} onChange={changeInput} type={'text'} name={'catch_phrase'} />
-      <br />
-      <label>images</label>
-      <input multiple={true} onChange={changeFileInput} type={'file'} name={'image'} />
-      <br />
-      <button onClick={addNewHero}>add new superhero</button>
+      <h1>Add new Superhero</h1>
+      
+      <div className={'form'}>
+        <label>Nickname</label>
+        <input value={state.nickname} onChange={changeInput} type={'text'} name={'nickname'} />
+        <br />
+        <label>Real Name</label>
+        <input value={state.real_name} onChange={changeInput} type={'text'} name={'real_name'} />
+        <br />
+        <label className={'labelTextarea'}>Origin Description</label>
+        <textarea value={state.origin_description} onChange={changeInput} name={'origin_description'} />
+        <br />
+        <label>Superpowers</label>
+        <input value={state.superpowers} onChange={changeInput} type={'text'} name={'superpowers'} />
+        <br />
+        <label>Catch Phrase</label>
+        <input value={state.catch_phrase} onChange={changeInput} type={'text'} name={'catch_phrase'} />
+        <br />
+        <label>Images</label>
+        <input multiple={true} onChange={changeFileInput} type={'file'} name={'image'} />
+        <br />
+        <button onClick={addNewHero}>add new superhero</button>
+      </div>
 
       { newSuperhero && <Redirect to={`/superhero/${newSuperhero.id}`} /> }
     </div>
